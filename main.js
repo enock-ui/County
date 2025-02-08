72911c368b6fd4834d7fe888d64d23f4c4322048
Enock Suter <enocksuter83@gmail.com>
2025-02-08 19:12:29 +0300

    fix: resolve crash when clicking submit button

--- main.js
+++ main.js
@@ -1,21 +1,14 @@
-var map = L.map('map').setView([ -1.319380, 36.769141], 13);
+var map = L.map('myMap').setView([ -1.319380, 36.769141], 13);
 
-L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
-    maxZoom: 20,
-    attribution: '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title'
-
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-fetch("https://raw.githubusercontent.com/sammigachuhi/geojson_files/main/counties_json.json")
-    .then((response) =>{
-        return response.json()
-    })
-    .then((data) => {
-        L.geoJson(data).bindPopup((layer) => {
-            return `City: ${layer.feature.properties.City},<br>
-            Population: ${layer.feature.properties.Population}`}).addTo(map);
-    })
-    .catch((error) => {
-        console.log(`This is the error: ${error}`)
-    })
+fetch("https://raw.githubusercontent.com/enock-ui/County/refs/heads/main_/Kenyancounties.json")
+
+.then(response => response.json())
+.then(data => {
+    L.geoJSON(data).addTo(map);
+})
+.catch(error => console.error('Error loading GeoJSON:', error));
 
